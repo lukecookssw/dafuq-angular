758b4472d8fa37eccb4402ebd5e95bdd2c2b4b0d
Luke Cook <lukecook@ssw.com.au>
2025-08-05 11:52:03 +0000

    debug

--- src/app/services/essays.service.ts
+++ src/app/services/essays.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +9,25 @@ export class EssaysService {
 
   constructor(private http: HttpClient) { }
 
-  getDiscussions() {
+  getDiscussions(): Observable<Discussion[]> {
     return this.http.get<any>('/api/github-discussions').pipe(
+      map(response => response.data.repository.discussions.nodes),
       catchError(error => {
         console.error('Error fetching discussions:', error);
         return throwError(() => new Error('Failed to fetch discussions'));
       })
     );
   }
+}
+
+
+export interface Discussion {
+  title: string;
+  bodyHtml: string;
+  url: string;
+  author: Author;
+}
+
+export interface Author {
+  login: string;
 }
