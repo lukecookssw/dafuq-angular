1634bd0e2cc7f51700abc0d97a32b430e4e5223a
Luke Cook <lukecook@ssw.com.au>
2025-08-09 02:40:22 +0000

    fix anchor styling in post

--- src/app/pages/tech/tech.ts
+++ src/app/pages/tech/tech.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { PageHeader } from '../../components/page-header/page-header';
+import { TextQuote } from '../../components/text-quote/text-quote';
 
 @Component({
   selector: 'app-tech',
-  imports: [],
+  imports: [PageHeader, TextQuote],
   templateUrl: './tech.html',
   styleUrl: './tech.scss'
 })
